64bcb40a976f912ad4a1d4209a9d2f52b23e5bee
KirtiRustagi <kirti.rustagi@truminds.com>
2022-02-03 19:08:12 +0530

    device list page

--- src/Pages/DeviceList/index.js
+++ src/Pages/DeviceList/index.js
@@ -3,9 +3,66 @@ import './deviceList.css';
 import { Card,Row,Col, Button } from 'antd';
 import 'antd/dist/antd.css';
 import {useLocation} from 'react-router-dom';
-
+import iOS from './img/ios.svg';
+import android from './img/android.svg';
 const DeviceList = (props) => {
-	
+	const list = [
+		{
+			name:'XYZ',
+			OS:'ios',
+			network:'4G'
+		},
+		{
+			name:'ABC',
+			OS:'android',
+			network:'5G'
+		},
+		{
+			name:'XYZ',
+			OS:'android',
+			network:'4G'
+		},
+		{
+			name:'XYZ',
+			OS:'ios',
+			network:'4G'
+		},
+		{
+			name:'XYZ',
+			OS:'ios',
+			network:'4G'
+		},
+		{
+			name:'XYZ',
+			OS:'ios',
+			network:'4G'
+		},
+		{
+			name:'XuZ',
+			OS:'android',
+			network:'5G'
+		},
+		{
+			name:'XYZ01',
+			OS:'android',
+			network:'4G'
+		},
+		{
+			name:'XYZ',
+			OS:'ios',
+			network:'4G'
+		},
+		{
+			name:'XYZ',
+			OS:'ios',
+			network:'4G'
+		},
+		{
+			name:'XYZ',
+			OS:'ios',
+			network:'4G'
+		},
+	];
 	const location = useLocation();
 	// functions
 	console.log(location.state)
@@ -13,14 +70,30 @@ const DeviceList = (props) => {
 		<div>
 			<div className='content'>
 				<Card className='card'>
-					<h1>bgygtyvh</h1>
+					<h1>Radio Site Info</h1>
 				</Card>
 			</div>
 			<div className='content'>
 				<Row justify='center'>
 					<Col span={24}>
 						<Card className='card'>
-							device list {location.state}
+							<div style={{textAlign:'center'}}>
+								<h1>Devices</h1>
+							</div>
+							<div>
+								<Row >
+									{list.map((item)=>{
+										return(
+											<Col style={{paddingLeft:20, margin:'2px 0px'}} span={8}>
+												{item.OS === 'ios'?
+													<img src={iOS} height={24}/>:
+													<img src={android} height={24}/>}
+												&nbsp;&nbsp;{item.name}
+											</Col>
+										)
+									})}
+								</Row>
+							</div>
 						</Card>
 					</Col>
 					
